629d1150158fcc02b2a974d449573b91d6a6c602
John Helmuth <john.helmuth@soundtransit.org>
2022-12-21 13:26:22 -0800

    add --include-all/-a CLI option to display the entire FeedMessage
    fixes #5

--- test.sh
+++ test.sh
@@ -8,6 +8,7 @@ cd "$(dirname $0)"
 set -x
 
 cat example-feed.pbf | ./cli.js --json | wc -l
+cat example-feed.pbf | ./cli.js --json --include-all | wc -l
 
 # FeedMessage with valid FeedHeader but 0 FeedEntitys
 empty="$(echo '0a 0d 0a 03 32 2e 30 10 00 18 9d ce c4 91 06' | xxd -r -p | ./cli.js)"
