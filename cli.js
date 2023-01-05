public-transport/gtfs-via-postgres

mri -> util.parseArgs

cli.js
@@ -1,24 +1,54 @@
 #!/usr/bin/env node
 'use strict'
 
-const mri = require('mri')
+const {parseArgs} = require('util')
 const pkg = require('./package.json')
 
-const argv = mri(process.argv.slice(2), {
-	boolean: [
-		'help', 'h',
-		'version', 'v',
-		'silent', 's',
-		'require-dependencies', 'd',
-		'ignore-unsupported', 'u',
-		'trips-without-shape-id',
-		'stops-without-level-id',
-		'stops-location-index',
-		'postgraphile',
-	]
+const {
+	values: flags,
+	positionals: args,
+} = parseArgs({
+	options: {
+		'help': {
+			type: 'boolean',
+			short: 'h',
+		},
+		'version': {
+			type: 'boolean',
+			short: 'v',
+		},
+		'silent': {
+			type: 'boolean',
+			short: 's',
+		},
+		'require-dependencies': {
+			type: 'boolean',
+			short: 'd',
+		},
+		'ignore-unsupported': {
+			type: 'boolean',
+			short: 'u',
+		},
+		'trips-without-shape-id': {
+			type: 'boolean',
+		},
+		'stops-without-level-id': {
+			type: 'boolean',
+		},
+		'stops-location-index': {
+			type: 'boolean',
+		},
+		'schema': {
+			type: 'string',
+		},
+		'postgraphile': {
+			type: 'boolean',
+		},
+	},
+	allowPositionals: true,
 })
 
-if (argv.help || argv.h) {
+if (flags.help) {
 	process.stdout.write(`
 Usage:
     gtfs-to-sql [options] [--] <gtfs-file> ...
@@ -44,7 +74,7 @@ Examples:
 	process.exit(0)
 }
 
-if (argv.version || argv.v) {
+if (flags.version) {
 	process.stdout.write(`${pkg.name} v${pkg.version}\n`)
 	process.exit(0)
 }
@@ -53,24 +83,22 @@ const {basename, extname} = require('path')
 const {pipeline} = require('stream')
 const convertGtfsToSql = require('./index')
 
-const files = argv._.map((file) => {
+const files = args.map((file) => {
 	const name = basename(file, extname(file))
 	return {name, file}
 })
 
 const opt = {
-	silent: !!(argv.silent || argv.s),
-	requireDependencies: !!(argv['require-dependencies'] || argv.d),
-	ignoreUnsupportedFiles: !!(argv['ignore-unsupported'] || argv.u),
-	tripsWithoutShapeId: !!argv['trips-without-shape-id'],
-	routesWithoutAgencyId: !!argv['routes-without-agency-id'],
-	stopsLocationIndex: !!argv['stops-location-index'],
-	schema: argv['schema'] || 'public',
-	postgraphile: !!argv.postgraphile,
+	silent: !!flags.silent,
+	requireDependencies: !!flags['require-dependencies'],
+	ignoreUnsupportedFiles: !!flags['ignore-unsupported'],
+	tripsWithoutShapeId: !!flags['trips-without-shape-id'],
+	routesWithoutAgencyId: !!flags['routes-without-agency-id'],
+	stopsLocationIndex: !!flags['stops-location-index'],
+	schema: flags['schema'] || 'public',
+	postgraphile: !!flags.postgraphile,
 }
-if ('stops-without-level-id' in argv) {
-	opt.stopsWithoutLevelId = !!argv['stops-without-level-id']
-}
+opt.stopsWithoutLevelId = !flags['stops-without-level-id']
 
 pipeline(
 	convertGtfsToSql(files, opt),

derhuerst/print-gtfs-rt-cli

cli.js
@@ -1,29 +1,53 @@
 #!/usr/bin/env node
 'use strict'
 
-const mri = require('mri')
+const {parseArgs} = require('util')
 const {isatty} = require('tty')
 
 const pkg = require('./package.json')
 
-const argv = mri(process.argv.slice(2), {
-	boolean: [
-		'help', 'h',
-		'version', 'v',
-		'length-prefixed', 'l',
-		'json', 'j',
-		'single-json', 's',
-	]
+const {
+	values: flags,
+} = parseArgs({
+	options: {
+		help: {
+			type: 'boolean',
+			short: 'h',
+		},
+		version: {
+			type: 'boolean',
+			short: 'v',
+		},
+		'length-prefixed': {
+			type: 'boolean',
+			short: 'l',
+		},
+		json: {
+			type: 'boolean',
+			short: 'j',
+		},
+		'single-json': {
+			type: 'boolean',
+			short: 's',
+		},
+		'depth': {
+			type: 'string',
+			short: 'd',
+		},
+		'gtfs-rt-bindings': {
+			type: 'string',
+		},
+	},
 })
 
-if (argv.help || argv.h) {
+if (flags.help) {
 	process.stdout.write(`
 Usage:
     cat gtfs-rt-feed.pbf | print-gtfs-rt
 Options:
     --length-prefixed  -l  Read input as length-prefixed.
                            See https://www.npmjs.com/package/length-prefixed-stream
-    --json  -j             Output newline-delimeted JSON (http://ndjson.org).
+    --json             -j  Output newline-delimeted JSON (http://ndjson.org).
     --single-json -s       Output a single JSON array.
     --depth            -d  Number of nested levels to print. Default: infinite
     --gtfs-rt-bindings     Path to GTFS-RT bindings. Must be compatible with
@@ -34,7 +58,7 @@ Examples:
 	process.exit(0)
 }
 
-if (argv.version || argv.v) {
+if (flags.version) {
 	process.stdout.write(`print-gtfs-rt v${pkg.version}\n`)
 	process.exit(0)
 }
@@ -65,14 +89,14 @@ const read = (readable) => {
 	})
 }
 
-const isLengthPrefixed = argv['length-prefixed'] || argv.l
-const printAsNDJSON = argv.json || argv.j
-const printAsJSON = argv['single-json'] || argv.s
+const isLengthPrefixed = flags['length-prefixed']
+const printAsNDJSON = flags.json
+const printAsJSON = flags['single-json']
 const printWithColors = isatty(process.stdout.fd)
-const depth = argv.depth || argv.d ? parseInt(argv.depth || argv.d) : null
+const depth = flags.depth ? parseInt(flags.depth) : null
 
-const bindings = argv['gtfs-rt-bindings']
-	? require(pathResolve(process.cwd(), argv['gtfs-rt-bindings']))
+const bindings = flags['gtfs-rt-bindings']
+	? require(pathResolve(process.cwd(), flags['gtfs-rt-bindings']))
 	: defaultBindings
 
 const {FeedMessage} = bindings.transit_realtime || bindings